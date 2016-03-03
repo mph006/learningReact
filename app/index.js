6abed4a35f63fb24436b9a4432b555ab64d73b16
Matt Herman <matthew.p.herman@gmail.com>
2016-03-03 14:11:12 -0500

    Added React router and components

--- app/index.js
+++ app/index.js
@@ -1,46 +1,68 @@
-var USER_DATA = {
-	name : "Matt Herman",
-	username: "mph006",
-	image: 'https://avatars2.githubusercontent.com/u/3487290?v=3&u=776a16c28dce7037eb66fd2881635f78fe4e99bd&s=140'
-}
+// var USER_DATA = {
+// 	name : "Matt Herman",
+// 	username: "mph006",
+// 	image: 'https://avatars2.githubusercontent.com/u/3487290?v=3&u=776a16c28dce7037eb66fd2881635f78fe4e99bd&s=140'
+// }
 
 var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
 
-var ProfilePic = React.createClass({
-	render: function (){
-		return (<img src={this.props.imageUrl} style={{height:100, width:100}} />)
-	}
-});
+// var ProfilePic = React.createClass({
+// 	render: function (){
+// 		return (<img src={this.props.imageUrl} style={{height:100, width:100}} />)
+// 	}
+// });
 
-var ProfileLink = React.createClass({
-	render: function(){
-		return (
-			<div>
-				<a href={'https://www.github.com/'+this.props.username}>
-					{this.props.username}
-				</a>
-			</div>
-		)
-	}
-});
+// var Link = React.createClass({
 
-var UserName = React.createClass({
-	render: function(){
-		return (<div>{this.props.name}</div>)
-	}
-});
+// 	changeURL: function(){
+// 		window.location.replace(this.props.href);
+// 	},
 
-var Avatar = React.createClass({
-	render: function(){
-		return (
-			<div>
-				<ProfilePic imageUrl={this.props.user.image} />
-				<ProfileLink username={this.props.user.username} />
-				<UserName name={this.props.user.name} />
-			</div>
-		)
-	}
-});
+// 	render:function(){
+// 		return (
+// 			<span 
+// 				style={{color: 'blue', cursor:'pointer'}}
+// 				onClick={this.changeURL}>
+// 				{this.props.children}
+// 			</span>
+// 		)
+// 	}
+// });
 
-ReactDOM.render(<Avatar user={USER_DATA}/>,document.getElementById("app"));
+// var ProfileLink = React.createClass({
+// 	render: function(){
+// 		return (
+// 			<div>
+// 				<Link href={'https://www.github.com/'+this.props.username}>
+// 					{this.props.username}
+// 				</Link>
+// 			</div>
+// 		)
+// 	}
+// });
+
+// var UserName = React.createClass({
+// 	render: function(){
+// 		return (<div>{this.props.name}</div>)
+// 	}
+// });
+
+// var Avatar = React.createClass({
+// 	render: function(){
+// 		return (
+// 			<div>
+// 				<ProfilePic imageUrl={this.props.user.image} />
+// 				<ProfileLink username={this.props.user.username} />
+// 				<UserName name={this.props.user.name} />
+// 			</div>
+// 		)
+// 	}
+// });
+
+ReactDOM.render(
+	routes,
+	// <Avatar user={USER_DATA}/>,
+	document.getElementById("app")
+);
